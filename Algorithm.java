7a4f9ffc0866f678fe81a7ee35c562589dcbf090
seanadeymccaul <103879392+seanadeymccaul@users.noreply.github.com>
2022-05-16 09:56:26 +1000

    Specification changes

--- Algorithm.java
+++ Algorithm.java
@@ -1,9 +1,17 @@
 public class Algorithm {
 
+    public Algorithm(int xDimension, int yDimension){
+
+    }
+
     private int[] results;
 
     public int[] generateResults(){
         return new int[0];
+    }
+
+    public int[] getResults(){
+        return new int[1];
     }
 
 }
--- Maze.java
+++ Maze.java
@@ -1,6 +1,6 @@
 import java.sql.ResultSet;
 
-abstract class Maze implements MazeInterface{
+abstract class Maze {
 
     // Fields
     private int xDimension;
@@ -9,19 +9,32 @@ abstract class Maze implements MazeInterface{
     private int stepSize;
     private int borderDesign;
     public int[] mazeData;
+    private Solution solution;
+    private Algorithm algorithm;
 
+    /**
+     * @param xDimension width of the maze
+     * @param yDimension height of the maze
+     */
     public Maze(int xDimension, int yDimension){
         this.cells = xDimension * yDimension;
         this.mazeData = new int[cells];
-        GenerateAuto();
     }
 
-    // Methods
-    public int[] GenerateBlank() {
-        return new int[1];
+    /**
+     * Populates the maze data according to maze generation algorithm
+     */
+    public void GenerateAuto() {
+        this.algorithm = new Algorithm(xDimension,yDimension);
+        algorithm.generateResults();
+        this.mazeData = algorithm.getResults();
     }
 
-    public int[] GenerateAuto(){
+    /**
+     * 
+     * @return
+     */
+    public int[] GenerateBlank(){
         for (int i = 0; i < cells; i++){
             mazeData[i] = i%2;
         }
