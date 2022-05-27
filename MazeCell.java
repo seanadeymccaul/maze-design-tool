fa1644aa6d1cb27d241dcee8a97f28790843f15b
seanadeymccaul <103879392+seanadeymccaul@users.noreply.github.com>
2022-05-27 11:16:06 +1000

    Refactoring Adult/Child classes

--- MazeCell.java
+++ MazeCell.java
@@ -1,19 +1,49 @@
 public class MazeCell {
 
+    private int id;
+    private int index;
+    private int wallLeft;
+    private int wallRight;
+    private int wallAbove;
+    private int wallBelow;
 
+    public MazeCell(int id, int wallLeft, int wallRight, int wallAbove, int wallBelow, int index){
+        this.id = id;
+        this.index = index;
+        this.wallLeft = wallLeft;
+        this.wallRight = wallRight;
+        this.wallAbove = wallAbove;
+        this.wallBelow = wallBelow;
+    }
 
-    public int value;
-    public int valueAbove;
-    public int valueBelow;
-    public int valueLeft;
-    public int valueRight;
+    public boolean CheckIfTop(int xDimension, int yDimension){
+        if (this.index < xDimension){
+            return true;
+        }
+        return false;
+    }
 
-    public MazeCell(int value, int valueLeft, int valueRight, int valueAbove, int valueBelow){
-        this.value = value;
-        this.valueLeft = valueLeft;
-        this.valueRight = valueRight;
-        this.valueAbove = valueAbove;
-        this.valueBelow = valueBelow;
+    public boolean CheckIfBottom(int xDimension, int yDimension){
+        if (this.index >= xDimension * (yDimension - 1)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean CheckIfLeft(int xDimension, int yDimension){
+        if (this.index%xDimension == 0){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean CheckIfRight(int xDimension, int yDimension){
+        if (this.index%xDimension == xDimension - 1){
+            return true;
+        } else if (this.index + 1 == xDimension * yDimension){
+            return true;
+        }
+        return false;
     }
 
 }
